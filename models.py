8f95e26993a2ef0b906ad54c9317c7b9fee954c3
CreatureTech1 <113849335+CreatureTech1@users.noreply.github.com>
2023-01-14 18:22:54 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -1,16 +1,65 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from autoslug import AutoSlugField
+from django_countries.fields import CountryField
+from recruiters.models import Job
+from django.utils import timezone
+#https://dl.dropboxusercontent.com/s/vqp7i2tv6uvc1gs/logofenice.jfif?dl=0
 
-# Create your models here.
-class UserDetails(models.Model):
-    userid = models.IntegerField
-    username = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
+CHOICES = (
+    ('Full Time', 'Full Time'),
+    ('Part Time', 'Part Time'),
+    ('Internship', 'Internship'),
+    ('Remote', 'Remote'),
+)
 
 
-from django.db import models
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True, related_name='profile')
+    full_name = models.CharField(max_length=200, null=True, blank=True)
+    country = CountryField(null=True, blank=True)
+    location = models.CharField(max_length=255, null=True, blank=True)
+    resume = models.FileField(upload_to='resumes', null=True, blank=True)
+    grad_year = models.IntegerField(blank=True)
+    looking_for = models.CharField(
+        max_length=30, choices=CHOICES, default='Full Time', null=True)
+    slug = AutoSlugField(populate_from='user', unique=True)
 
-class FacebookResponse(models.Model):
-    facebook_id = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    email = models.EmailField()
-    
+    def get_absolute_url(self):
+        return "/profile/{}".format(self.slug)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Skill(models.Model):
+    skill = models.CharField(max_length=200)
+    user = models.ForeignKey(
+        User, related_name='skills', on_delete=models.CASCADE)
+
+
+class SavedJobs(models.Model):
+    job = models.ForeignKey(
+        Job, related_name='saved_job', on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, related_name='saved', on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.job.title
+
+
+class AppliedJobs(models.Model):
+    job = models.ForeignKey(
+        Job, related_name='applied_job', on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, related_name='applied_user', on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.job.title
